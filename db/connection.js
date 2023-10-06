19872b2ba95a91df181f6cf15bb1391aaa4d2cae
Kashish Desai <113315022+kashishdesai03@users.noreply.github.com>
2023-10-05 21:16:39 -0400

    test

--- db/connection.js
+++ db/connection.js
@@ -3,11 +3,11 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize({
-  port: process.env.DBPORT,
   database: process.env.DBNAME,
   username: process.env.DBUSER,
   password: process.env.DBPASSWORD,
   host: process.env.PORT,
+  port: process.env.DBPORT,
   dialect: "postgres",
 });
 
