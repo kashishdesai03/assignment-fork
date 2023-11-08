ac406306e9911d09bd3fa74a7eafaf4918ed82f9
Kashish Desai <113315022+kashishdesai03@users.noreply.github.com>
2023-11-08 00:20:23 -0500

    added logs

--- script.sh
+++ script.sh
@@ -25,6 +25,26 @@ cd webapp
 mv users.csv /opt/
 npm i
 
+# Download the CloudWatch Agent installer
+echo_info "DOWNLOADING-CLOUDWATCH-AGENT"
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
+
+# Install the CloudWatch Agent
+echo_info "INSTALLING-CLOUDWATCH-AGENT"
+sudo dpkg -i amazon-cloudwatch-agent.deb
+
+# Start the CloudWatch Agent service
+echo_info "STARTING-CLOUDWATCH-AGENT"
+sudo service amazon-cloudwatch-agent start
+
+# Enable the CloudWatch Agent to start on boot
+echo_info "ENABLING-CLOUDWATCH-AGENT"
+sudo systemctl enable amazon-cloudwatch-agent
+
+# Start CloudWatch Agent
+echo_info "STARTING-CLOUDWATCH-AGENT"
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
+
 # Starting the service
 
 sudo sh -c "echo '[Unit]
@@ -37,7 +57,7 @@ EnvironmentFile=/etc/environment
 Type=simple
 User=admin
 WorkingDirectory=/opt/
-ExecStart=/usr/bin/node /opt/app.js
+ExecStart=/usr/local/bin/node /opt/app.js
 Restart=always
 RestartSec=10
 
