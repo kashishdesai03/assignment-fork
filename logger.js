34e2fae7b11b695fbd4018a2e2e81d45b3148230
Kashish Desai <113315022+kashishdesai03@users.noreply.github.com>
2023-11-09 15:19:16 -0500

    added metrics

--- logger.js
+++ logger.js
@@ -1,12 +1,12 @@
 const winston = require("winston");
 
-const cloudwatchAgentLogPath = "/var/logs/amazon-cloudwatch-agent.log";
+//const cloudwatchAgentLogPath = "/var/logs/amazon-cloudwatch-agent.log";
 const webAppLogPath = "/opt/webapp/csye6225.log";
 
 const logger = winston.createLogger({
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: cloudwatchAgentLogPath }),
+    //new winston.transports.File({ filename: cloudwatchAgentLogPath }),
     new winston.transports.File({ filename: webAppLogPath }),
   ],
 });
